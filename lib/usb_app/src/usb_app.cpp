8973e57acdb0789cbdfd199695a1c314024529c1
wass <wassim.filali@gmail.com>
2020-11-28 21:04:36 +0100

    bringing the usb device along

--- lib/usb_app/src/usb_app.cpp
+++ lib/usb_app/src/usb_app.cpp
@@ -1,9 +1,16 @@
 #include "usb_app.h"
 #include "stm32f4xx_hal.h"
 
+#include "usbd_core.h"
+
+USBD_HandleTypeDef USBD_Device;
+
+
 bool UsbApp::init()
 {
-  HAL_Delay(100);
+
+  //USBD_Init(&USBD_Device, &AUDIO_Desc, 0);
+  USBD_Init(&USBD_Device, NULL, 0);
 
   return true;
 }
